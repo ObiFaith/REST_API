9eb11097f7220b70995b84953416c76fa8373934
ObiFaith <obifth@gmail.com>
2024-05-30 17:35:49 +0100

    Added theme.js

--- script/index.js
+++ script/index.js
@@ -73,21 +73,3 @@ const displayContent = async (pageNo = 1, countriesPerPage = 27) => {
 };
 
 displayContent();
-
-const toggleDarkMode = () => {
-    const body = document.body;
-    body.classList.toggle('dark-mode');
-    // Save the current theme in localStorage
-    const isDarkMode = body.classList.contains('dark-mode');
-    localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
-};
-
-// Function to load the saved theme
-const loadTheme = () => {
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme && savedTheme === 'dark') document.body.classList.add('dark-mode');
-    else document.body.classList.remove('dark-mode');
-};
-
-window.addEventListener('DOMContentLoaded', loadTheme);
-document.getElementById('theme-toggle').addEventListener('click', toggleDarkMode);
